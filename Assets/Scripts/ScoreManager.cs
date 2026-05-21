ea24538b9265b7dd5c9b482dfb2f28fa29a00e2e
Crmy7 <114735818+Crmy7@users.noreply.github.com>
2026-05-21 15:31:43 +0200

    feat: enhance ScoreManager to track and save best score

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -4,13 +4,31 @@ using UnityEngine;
 public class ScoreManager : MonoBehaviour
 {
     private int _score;
+    private int _bestScore;
 
     // Propriété en lecture seule pour que l'UI affiche le score.
     public int Score => _score;
+    public int BestScore => _bestScore;
 
+    private void Awake()
+    {
+        _bestScore = PlayerPrefs.GetInt("BestScoreKey", 0);
+    }
+    
     public void IncrementScore()
     {
         _score++;
+        TrySaveBestScore();
+    }
+
+    public void TrySaveBestScore()
+    {
+        if (_score > _bestScore)
+        {
+            _bestScore = _score;
+            PlayerPrefs.SetInt("BestScoreKey", _score);
+            PlayerPrefs.Save();
+        }
     }
 
     public void ResetScore()

0838f86d52a25e3041b33ef729dccfceb086662f
Crmy7 <114735818+Crmy7@users.noreply.github.com>
2026-05-21 16:32:59 +0200

    refactor: replace pickupSound field with data-driven approach using RupeeData

--- Assets/Scripts/Rupee.cs
+++ Assets/Scripts/Rupee.cs
@@ -1,13 +1,29 @@
 using System;
 using UnityEngine;
 
+[RequireComponent(typeof(SpriteRenderer))]
 // Une rupee individuelle. Quand le joueur la touche, elle fire OnCollected puis se détruit.
 public class Rupee : MonoBehaviour
 {
-    [SerializeField] private AudioClip pickupSound;
     
     // Event écouté par RupeeManager pour tracker la collecte.
     public event Action<Rupee> OnCollected;
+
+    private RupeeData _data;
+    private SpriteRenderer _spriteRenderer;
+    
+    public RupeeData Data => _data;
+
+    public void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void Init(RupeeData data)
+    {
+        _data = data;
+        _spriteRenderer.color = _data.color;
+    }
 
     // Appelée automatiquement quand un autre Collider2D (en trigger) entre dans le trigger de cette rupee.
     private void OnTriggerEnter2D(Collider2D other)
@@ -25,9 +41,9 @@ public class Rupee : MonoBehaviour
         var audioGo = new GameObject("PickupSFX");
         audioGo.transform.position = transform.position;
         var source = audioGo.AddComponent<AudioSource>();
-        source.clip = pickupSound;
+        source.clip = _data.pickupSound;
         source.spatialBlend = 0f;
         source.Play();
-        Destroy(audioGo, pickupSound.length);
+        Destroy(audioGo, _data.pickupSound.length);
     }
 }
